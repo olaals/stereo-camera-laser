b0003dc86b33a9c2a2252bcfae6c456cacce38be
olaals <olaalstad96@gmail.com>
2023-01-20 16:08:06 +0100

    Add progress

--- stereo_filter.py
+++ stereo_filter.py
@@ -1,3 +1,4 @@
+import os
 from harvesters.core import simplefilter
 from genicam_wrappers import GenicamStereo
 import cv2
@@ -11,6 +12,10 @@ def read_json(json_path):
         data = json.load(f)
     return data
 
+def save_json(json_path, data):
+    with open(json_path, 'w') as f:
+        json.dump(data, f)
+
 def read_calib(calib_path):
     calib_dict = read_json(calib_path)
     H = np.array(calib_dict["H"])
@@ -21,16 +26,16 @@ def read_calib(calib_path):
     T_ltr = np.array(calib_dict['T_ltr'])
     return lK, ldc, rK, rdc, T_ltr, H
 
-
-
 def apply_planar_homography(H, img):
     projected_img = cv2.warpPerspective(img, H, (img.shape[1], img.shape[0]))
     return projected_img
 
-
-def capture_homography():
-    cti_path = "/opt/cvb-13.04.001/drivers/genicam/libGevTL.cti.1.2305"
-    calib_path = "/home/ola/projects/stereo-camera-laser/calibration/calib-11-01/calib.json"
+def capture_homography(cti_path, calib_path, exposure_time, save_dir):
+    os.makedirs(save_dir)
+    left_save_dir = os.path.join(save_dir, 'left')
+    right_save_dir = os.path.join(save_dir, 'right')
+    os.makedirs(left_save_dir)
+    os.makedirs(right_save_dir)
     calib_dict = read_json(calib_path)
     lK, ldc, rK, rdc, T_ltr, H = read_calib(calib_path)
     print("lK", lK)
@@ -39,31 +44,42 @@ def capture_homography():
     print("rdc", rdc)
     print("T_ltr", T_ltr)
     print("H",H)
-    exposure_time = 10000
+    exposure_time = 30000
     device_num_left = 1
     device_num_right = 0
     cap = GenicamStereo(cti_path, device_num_left, device_num_right, exposure_time, gray=True)
+    save_num = 0
     while True:
         left, right = cap.read()
         # undist left and right
         left = cv2.undistort(left, lK, ldc)
         right = cv2.undistort(right, rK, rdc)
         projected = apply_planar_homography(H, right)
-        thresh = 50
-        threshed = np.bitwise_and(projected>50,left>50)
+        thresh = 30
+        threshed = np.bitwise_and(projected>thresh,left>thresh)
         simple_filter = np.where(threshed, left, 0)
         depth_stack = np.dstack((left, projected, projected))
-        #cv2.imshow('left', left)
-        #cv2.imshow('right', right)
+        cv2.imshow('left', left)
+        cv2.imshow('right', right)
         cv2.imshow('projected', depth_stack)
         cv2.imshow('filtered', simple_filter)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        key = cv2.waitKey(1)
+        # save on keypress s
+        if key == ord('s'):
+            print("saving")
+            left_name = os.path.join(left_save_dir, f'left_{save_num}.png')
+            right_name = os.path.join(right_save_dir, f'right_{save_num}.png')
+            cv2.imwrite(left_name, left)
+            cv2.imwrite(right_name, right)
+
 
 
 
 
 
 if __name__ == '__main__':
-    capture_homography()
+    cti_path = "/opt/cvb-13.04.005/drivers/genicam/libGevTL.cti.1.2309"
+    calib_path = "calibration/calib-11-01/calib.json"
+    exposure_time = 25000
+    save_dir = "/data"
+    capture_homography(cti_path, calib_path, exposure_time, )
